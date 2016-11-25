c522f71a6dd16cd33f259233e039babfc3d5a6c0
pand0raQ <Halik@Haliks-MacBook-Air.local>
2016-11-25 15:07:05 +0400

    Initial Commit

---
+++ pribblbe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pribblbe
+//
+//  Created by Halik Magomedov on 11/25/16.
+//  Copyright © 2016 Haliks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
